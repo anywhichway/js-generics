44691c87defc16cde50b7583cf6ea0687d067002
Simon Blackwell <syblackwell@anywhichway.com>
2016-01-03 18:16:06 -0500

    Added unit tests and documentation.

---
+++ test/index.js
@@ -0,0 +1,28 @@
+var expect;
+if(typeof(window)==="undefined") {
+	expect = require("chai").expect;
+}
+
+describe('generic', function() {
+	var sum = generic(function() { throw new Error("can't sum " + JSON.stringify(arguments)); } )
+	.method("number","number",
+			 function(arg1,arg2) { return arg1 + arg2; })
+	.method(function(arg) { return arg instanceof Array;},"number",
+			 function(arg1, arg2) { arg1.forEach(function(item,i) { arg1[i] += arg2; }); return arg1; });
+	it('number sum ', function() {
+		expect(sum(1,2)).to.be.equal(3);
+	});
+	it('array number sum ', function() {
+		var result = sum([0,1],2)
+		expect(result[0]===2 && result[1]===3).to.be.true;
+	});
+	it('should throw error ', function() {
+		var result;
+		try {
+			result = sum(null,1);
+		} catch(e) {
+			result = e;
+		}
+		expect(result).to.be.instanceof(Error);
+	});
+});
